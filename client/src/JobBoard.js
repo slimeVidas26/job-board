4d3df6130605ec5e2600585ccbb58e8289c7ad4d
SlimeVidas <dahan.isaac@gmail.com>
2021-09-29 10:49:21 +0300

    jobBoard-stateless

--- client/src/JobBoard.js
+++ client/src/JobBoard.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState , useEffect } from 'react';
 import { JobList } from './JobList';
 import { getAllJobs } from './request';
 
 
 
-export class JobBoard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {jobs : []}
-  }
-
-  async componentDidMount(){
-  const jobs = await getAllJobs();
-  this.setState({jobs})
-  }
+export function JobBoard() {
   
-  render() {
-    const {jobs} = this.state;
+
+  const [jobs , setJobs] = useState([])
+
+
+  useEffect(() => {
+      getAllJobs()
+     .then((jobs)=>{setJobs(jobs)
+     } )
+  }, [])
+  
+  
+    
     return (
       <div>
         <h1 className="title">Job Board</h1>
+        {jobs &&
         <JobList jobs={jobs} />
+        }
       </div>
     );
-  }
+  
 }
--- client/src/request.js
+++ client/src/request.js
@@ -1,24 +1,26 @@
 const endpointUrl = 'http://localhost:9000/graphql';
 
+const options = {
+    method : 'POST',
+    headers : {
+      'content-type':'application/json'
+    },
+    body : JSON.stringify({
+      query : `query{
+        jobs {
+          id
+          title
+          description
+        company {
+          id
+        }
+        }
+      }`
+    })
+  }
+
 export const getAllJobs = async ()=>{
-const response = await fetch(endpointUrl , {
-  method : 'POST',
-  headers : {
-    'content-type':'application/json'
-  },
-  body : JSON.stringify({
-    query : `query{
-      jobs {
-        id
-        title
-        description
-      company {
-        id
-      }
-      }
-    }`
-  })
-});
+const response = await fetch(endpointUrl , options);
 const responseBody = await response.json();
 return responseBody.data.jobs
 }
